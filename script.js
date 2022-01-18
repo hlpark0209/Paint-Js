6d700c4f810340cbdde07dda92f615d4ba84cdf3
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-19 02:16:38 +0900

    Update canvas path function

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 'use strict';
 const canvas = document.querySelector('#jsCanvas');
+const ctx = canvas.getContext("2d");
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#000000";
+ctx.lineWidth = 2.5;
+
+
 let painting = false;
 
 // 그리기 중지
 function stopPainting(){
     painting = false;
 }
+function startPainting(){
+    painting = true;
+}
 
 // 마우스가 움직일때
 function onMouseMove(e){
     const x = e.offsetX;
     const y = e.offsetY;
-    // console.log(x, y);
+    
+    // path를 생성함 -> 선으로 시작점을 만듦, 클릭을 했을때는 path가 종료
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 // 클릭했을때 
@@ -19,14 +38,10 @@ function onMouseDown(e){
     painting = true;
 }
 
-// 클릭을 땠을떄
-function onMouseUp(event){
-    stopPainting();
-}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
