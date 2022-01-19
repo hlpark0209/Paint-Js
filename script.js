4205d13ff604117bef9f08dea764bc91e47e6f65
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-19 16:54:57 +0900

    Update fill+ save button

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 const canvas = document.querySelector('#jsCanvas');
 const ctx = canvas.getContext("2d");
 const eachColors = document.querySelectorAll('.controls__color');
+const jsRange = document.querySelector('#jsRange');
+const mode = document.querySelector("#jsMode");
+
 canvas.width = 700;
 canvas.height = 700;
 
 ctx.strokeStyle = "#000000";
 ctx.lineWidth = 2.5;
 
-
 let painting = false;
+let filling = false;
 
 // 그리기 중지
 function stopPainting(){
@@ -39,20 +42,47 @@ function onMouseDown(e){
     painting = true;
 }
 
+//색상 변경
 
+function changeColor(e){
+    const bgColor = e.target.style.backgroundColor;
+    ctx.strokeStyle = bgColor;
+}
+Array.from(eachColors)
+.forEach(color => color.addEventListener("click", changeColor));
+
+// 선두께 설정
+function handleRange(e){
+    const strokeSize = e.target.value;
+    ctx.lineWidth = strokeSize;
+}
+
+//fill + save 버튼 설정
+function handleMode(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill"
+    }else{
+        filling = true;
+        mode.innerText = "Paint"
+        
+    }
+}
+
+
+
+
+//함수 호출
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
-
-
-function changeColor(e){
-    const bgColor = e.target.style.backgroundColor;
-    ctx.strokeStyle = bgColor;
-    console.log(bgColor);
+if(jsRange){
+    jsRange.addEventListener('input', handleRange);
 }
 
-Array.from(eachColors)
-.forEach(color => color.addEventListener("click", changeColor));
+if(mode){
+    mode.addEventListener("click", handleMode);
+}
